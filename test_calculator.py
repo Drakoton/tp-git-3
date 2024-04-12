6c3b473e91580610320eafcd6bf17a7a145079e6
Dorian RAKOTONANAHARY <drakotonanahary@gmail.com>
2024-04-12 17:40:36 +0200

    pytest

---
+++ test_calculator.py
@@ -0,0 +1,9 @@
+from calculator import substraction
+
+from calculator import division
+
+def test_substraction():
+    assert substraction(1, 2) == -1
+
+def test_division():
+    assert division(1, 2) == 0.5
